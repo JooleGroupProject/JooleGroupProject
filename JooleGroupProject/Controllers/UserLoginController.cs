f33770fe87476bd7967ae107585370ab2889ef38
Kyle Schaffer <kyle.schaffer@gmail.com>
2021-10-29 11:02:27 -0400

    Fixed csproj

--- JooleGroupProject/Controllers/UserLoginController.cs
+++ JooleGroupProject/Controllers/UserLoginController.cs
@@ -48,7 +48,7 @@ namespace JooleGroupProject.Controllers
                     var userName = Session["User_Name"];
                     var userEmail = Session["User_Email"];
                     //return to searching page
-                    return RedirectToAction("Index", "Home");
+                    return RedirectToAction("ProductSummary", "Product");
                 }
             }
 
