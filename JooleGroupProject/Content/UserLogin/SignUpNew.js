281b00087fe2e32dffd5add3ffebf695f85d70e3
zhengcong xiao <zxiao13@hawk.iit.edu>
2021-10-31 20:13:27 -0400

    Update LoginPage

---
+++ JooleGroupProject/Content/UserLogin/SignUpNew.js
@@ -0,0 +1,9 @@
+﻿/*Preview Image*/
+var previewimg = function (event) {
+    var result = document.getElementById("imgpreview");
+    result.src = URL.createObjectURL(event.target.files[0]);
+};
+
+
+
+
--- JooleGroupProject/Controllers/UserLoginController.cs
+++ JooleGroupProject/Controllers/UserLoginController.cs
@@ -11,7 +11,6 @@ namespace JooleGroupProject.Controllers
 {
     public class UserLoginController : Controller
     {
-        
         // GET: UserLogin
         public ActionResult LoginPage()
         {
@@ -55,12 +54,6 @@ namespace JooleGroupProject.Controllers
 
         }
 
-
-        public ActionResult SignUpPage()
-        {
-            return View();
-        }
-
         [HttpPost]
         public ActionResult SignUp(tblUser tUser)
         {
@@ -71,28 +64,38 @@ namespace JooleGroupProject.Controllers
                     if (jdb.tblUsers.Any(userName => userName.User_Name == tUser.User_Name))
                     {
                         ViewBag.DuplicateMessage = "The User Name already exist!";
-                        return View("SignUpPage", tUser);
+                       //Session["User_name"] = tUser.User_Name;
+                       //var em = Session["User_name"].ToString();
+                       //ViewBag.DuplicateEmail = "The Email Exists, The User Name is " + em;
+                        return View("LoginPage", tUser);
+                    }else if (jdb.tblUsers.Any(userEmail => userEmail.User_Email == tUser.User_Email))
+                    {
+                        var obj = jdb.tblUsers.Where(userDetail => userDetail.User_Email == tUser.User_Email).Select(userName => userName.User_Name).SingleOrDefault();
+                        if (obj != null)
+                        {
+                            ViewBag.DuplicateEmail = "The Email already exist! and Your User Name: " + obj;
+                        }
+                        return View("LoginPage", tUser);
                     }
-                    jdb.tblUsers.Add(tUser);
-                    jdb.SaveChanges();
+                    else
+                    {
+                        HttpPostedFileBase file = Request.Files["Imgfile"];
+                        if (file != null && file.ContentLength != 0)
+                        {
+                            string fileName = Path.GetFileNameWithoutExtension(file.FileName);
+                            string extension = Path.GetExtension(file.FileName);
+                            fileName = fileName + DateTime.Now.ToString("yymmssff") + extension;
+                            tUser.User_Image = fileName;
+                            tUser.Imgfile.SaveAs(Path.Combine(Server.MapPath("~/Images"), fileName));
+                        }
+                            jdb.tblUsers.Add(tUser);
+                            jdb.SaveChanges();
+                    }
                 }
                 tUser = null;
                 ViewBag.SuccessMessage = "Registration Successful!";
             }
-            return View("SignUpPage", tUser);
-        }
-
-        public ActionResult SignUp(HttpPostedFileBase imgfile)
-        {
-            if (imgfile != null && imgfile.ContentLength > 0)
-            {
-                string imgname = Path.GetFileName(imgfile.FileName);
-                string imgext = Path.GetExtension(imgname);
-                string imgpath = Path.Combine(Server.MapPath("~/Images"), imgname);
-                imgfile.SaveAs(imgpath);
-
-            }
-            return View();
+            return View("LoginPage", tUser);
         }
     }
 }
